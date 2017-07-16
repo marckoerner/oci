2f754efe59c893c6418bba9585fbfbc5f8d05f80
marckoerner <koerner@icsi.berkeley.edu>
2017-07-16 16:45:20 -0700

    initial commit mock edge service

---
+++ oci-ces-mock-edge/src/main/java/oci/mock/edge/EchoEdgeService.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package oci.mock.edge;
+
+
+import oci.lib.CesEdgeSocket;
+import oci.template.EdgeDiscoveryService;
+
+/**
+ * @author marc
+ *
+ */
+public class EchoEdgeService extends EdgeDiscoveryService {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
