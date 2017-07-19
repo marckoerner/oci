a957718b51aa9d5fa2fba41bef2ca59349da5a21
marckoerner <koerner@icsi.berkeley.edu>
2017-07-19 11:44:30 -0700

    added an object class for service name entries

---
+++ libCES/src/main/java/oci/lib/ServiceNameEntry.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package oci.lib;
+
+import java.net.InetAddress;
+
+/**
+ * @author marc
+ *
+ */
+public class ServiceNameEntry {
+	
+	private String		serviceName	= null;
+	private InetAddress ip			= null;
+	
+	
+	public ServiceNameEntry(String serviceName, InetAddress ip) {
+		this.serviceName	= serviceName;
+		this.ip				= ip;	
+	}
+	
+	public InetAddress getIpAddress() {
+		return this.ip;
+	}
+	
+	public String getServiceName() {
+		return this.serviceName;
+	}
+
+} // class ServiceNameEntry
