60e11466c200b25f0eca4403401aa29aab633093
marckoerner <koerner@icsi.berkeley.edu>
2017-07-26 19:04:15 +0200

    created ServiceNameResolver class and port entry

---
+++ libCES/src/main/java/oci/lib/ServiceNameRegistration.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package oci.lib;
+
+/**
+ * @author marc
+ *
+ */
+public class ServiceNameRegistration {
+	
+	public final static int				PORT			= ServiceNameResolver.PORT + 1; // Port 5534
+
+} // class ServiceNameRegistration
