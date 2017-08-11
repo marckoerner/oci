46f194c8c156ffa46f09ad154f7cb3e9619ea04a
marckoerner <koerner@icsi.berkeley.edu>
2017-08-10 17:52:52 -0700

    fixed some runtime issues and added ls command

--- local-oci-coordinator/src/main/java/oci/locic/LocalOciCoordinator.java
+++ local-oci-coordinator/src/main/java/oci/locic/LocalOciCoordinator.java
@@ -23,7 +23,7 @@ public class LocalOciCoordinator {
 	
     static final Logger LOGGER = Logger.getLogger(LocalOciCoordinator.class.getName());
     /**
-	 * vector with discovery service name to IP entries
+	 * vector with discovery service name to IP mapping entries
 	 */
     static Vector<ServiceNameEntry> serviceList = new Vector<ServiceNameEntry>();
 
@@ -45,37 +45,51 @@ public class LocalOciCoordinator {
 			echoService = new ServiceNameEntry("mockService", InetAddress.getByAddress(ServiceNameResolver.IPADDRESS));
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
-			LocalOciCoordinator.serviceList.add(echoService);
 		}
+		
+		// add mock service static entry
+		LocalOciCoordinator.serviceList.add(echoService);
 		
 		try {
 			// start service registration thread
-			LOGGER.info("Try to open a server socket OCI name resolution port (" + ServiceNameRegistration.PORT + ")");
+			LOGGER.info("Try to open OCI name registraion port (" + ServiceNameRegistration.PORT + ")");
 			ServerSocket serviceRegistrationSocket = new ServerSocket(ServiceNameRegistration.PORT);
 			LOGGER.info("Successful");
 			ServiceNameRegistrationThread serviceRegistrationWorker = new ServiceNameRegistrationThread(serviceRegistrationSocket);
 			serviceRegistrationWorker.start();
 			
 			// start service resolver thread
-			LOGGER.info("Try to open a server socket OCI name resolution port (" + ServiceNameResolver.PORT + ")");
+			LOGGER.info("Try to open a OCI name resolution port (" + ServiceNameResolver.PORT + ")");
 			ServerSocket serviceResolverSocket = new ServerSocket(ServiceNameResolver.PORT);
 			LOGGER.info("Successful");
 			ServiceNameResolverThread serviceResolverWorker = new ServiceNameResolverThread(serviceResolverSocket);
+			serviceResolverWorker.start();
+
 								
 			// read from cmd and wait for command EXIT or statistics
 			InputStreamReader	inputStreamReader	= new InputStreamReader(System.in);
 			BufferedReader		stdIn		 		= new BufferedReader(inputStreamReader);					
 			String inputLine;
 			while ((inputLine = stdIn.readLine()) != null) {
-				System.out.println(inputLine);
+				// System.out.println(inputLine);
+				
+				// Exit
 				if(inputLine.equals("exit")) {
 					// shut down all serverSockets and wait until worker threads terminate (registration + resolver)
 					serviceRegistrationSocket.close(); // interrupts accept() method within thread implementation
 					serviceRegistrationWorker.join();
 					serviceResolverSocket.close();
 					serviceResolverWorker.join();
-					return;
-				}
+					break;
+				} //if exit
+				
+				// List service entries
+				if(inputLine.equals("ls")) {
+					for(int i = 0; i < LocalOciCoordinator.serviceList.size(); i++) {
+						System.out.println(serviceList.get(i).toString());
+					}
+				} // if list
+				
 			} // while
 			
 		} catch(Exception error) {
@@ -85,7 +99,7 @@ public class LocalOciCoordinator {
 			
 		} // try - catch
 		
-		LOGGER.info("LOCIC stopped");
+		LOGGER.info("Local OCI Coordinator stopped");
 
 	} // main
 
