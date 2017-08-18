904549fba653572971310c58475762592ac7fb84
marckoerner <koerner@icsi.berkeley.edu>
2017-08-18 16:40:29 -0700

    deleted empty lines

--- local-oci-coordinator/src/main/java/oci/locic/ServiceNameResolverThread.java
+++ local-oci-coordinator/src/main/java/oci/locic/ServiceNameResolverThread.java
@@ -19,11 +19,9 @@ public class ServiceNameResolverThread extends Thread {
 	
 	private ServerSocket	serverSocket	= null;
 	
-	
 	public ServiceNameResolverThread(ServerSocket serverSocket) {
 		this.serverSocket = serverSocket;
 	}
-	
 	
 	@Override 
 	public void run() {
