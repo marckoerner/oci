c91593d54baaf545bf91ecb5a16100da6e70ceb9
Torsten Runge <runge@icsi.berkeley.edu>
2017-08-18 18:15:33 -0700

    distribute binary from GC to one LC

--- gocic/src/main/java/oci/gocic/FileResource.java
+++ gocic/src/main/java/oci/gocic/FileResource.java
@@ -1,9 +1,11 @@
 package oci.gocic;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 
 import javax.ws.rs.Consumes;
@@ -21,42 +23,38 @@ import javax.ws.rs.core.Response.ResponseBuilder;
 import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
 import org.glassfish.jersey.media.multipart.FormDataParam;
 
-//import oci.gocic.messenger.model.File;
-//import oci.gocic.messenger.service.FileService;
-
 @Path("/files")
 @Consumes(MediaType.MULTIPART_FORM_DATA)
 @Produces(MediaType.MULTIPART_FORM_DATA)
 public class FileResource {
 
-	//	private FileService fileService = new FileService();	
-
-	private static final String FILE_PATH = "C:\\test\\";
-	private static final String UPLOAD_PATH = "C://upload//";
-
-	//	@GET
-	//	public List<File> getFiles() {
-	//		return fileService.getAllFiles();
-	//	}
+	//	private static final String ThridParty_PATH = "C://oci-test//ThridParty";
+	private static final String GC_PATH = "C:\\oci-test\\GC\\";
+	private static final String LC1_PATH = "C:\\oci-test\\LC1\\";
+	private static final String LC2_PATH = "C:\\oci-test\\LC2\\";
 
 	@GET
 	@Path("/{fileName}")
 	public Response getFile(@PathParam("fileName") String fileName) {
 
-		File file = new File(FILE_PATH + fileName);
+		File file = new File(GC_PATH + fileName);
 		ResponseBuilder response = Response.ok((Object) file);
 		response.header("Content-Disposition",
 				"attachment; filename=image_from_server.png");
 		return response.build();
 	}
 
+	//TODO: REST POST must handover multiple parameters: edge service binary and metadata
+	
 	@POST
 	public Response addFile(
 			@FormDataParam("file") InputStream uploadedInputStream,
-			@FormDataParam("file") FormDataContentDisposition fileDetail) {		
+			@FormDataParam("file") FormDataContentDisposition fileDetail) throws IOException {
 
-		try {
-			OutputStream out = new FileOutputStream(new File(UPLOAD_PATH + fileDetail.getFileName()));
+		String gcFilePath = GC_PATH + fileDetail.getFileName();
+		
+		try {	
+			OutputStream out = new FileOutputStream(new File(gcFilePath));
 			int read = 0;
 			byte[] bytes = new byte[1024];
 
@@ -69,8 +67,44 @@ public class FileResource {
 			e.printStackTrace();
 		}
 
-		String output = "File uploaded to : " + UPLOAD_PATH + fileDetail.getFileName();
+		String output = "File uploaded to : " + GC_PATH + fileDetail.getFileName();
+
+		distributeToLocalCoordinators(gcFilePath, LC1_PATH);
+
 		return Response.status(200).entity(output).build();
+	}
+
+	// distribute to LCs as defined in metadata 
+	//TODO: (so mapping of LC to IPs is needed)
+	private void distributeToLocalCoordinators(String gcFilePath, String lc1FilePath) {
+		String s = null;
+
+		try {
+			// run the copy command using the Runtime exec method:
+			//TODO: should be for Linux with the help of scp
+			String command = "cmd.exe /C copy " + gcFilePath.toString() + " " + lc1FilePath.toString();
+			Process p = Runtime.getRuntime().exec(command);
+
+			BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+
+			// read the output from the command
+			System.out.println("Here is the standard output of the command:\n");
+			while ((s = stdInput.readLine()) != null) {
+				System.out.println(s);
+			}
+
+			// read any errors from the attempted command
+			System.out.println("Here is the standard error of the command (if any):\n");
+			while ((s = stdError.readLine()) != null) {
+				System.out.println(s);
+			}
+
+		}
+		catch (IOException e) {
+			System.out.println("exception happened - here's what I know: ");
+			e.printStackTrace();
+		}
 	}
 
 	@PUT
@@ -78,13 +112,13 @@ public class FileResource {
 	public Response updateFile(
 			@FormDataParam("file") InputStream uploadedInputStream,
 			@FormDataParam("file") FormDataContentDisposition fileDetail) {
-		
+
 		File file = new File(fileDetail.getFileName());
-		
+
 		if (file.exists()) {
-			
+
 			try {
-				OutputStream out = new FileOutputStream(new File(UPLOAD_PATH + fileDetail.getFileName()));
+				OutputStream out = new FileOutputStream(new File(GC_PATH + fileDetail.getFileName()));
 				int read = 0;
 				byte[] bytes = new byte[1024];
 
@@ -96,14 +130,14 @@ public class FileResource {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			
-			String output = "File updated: " + UPLOAD_PATH + fileDetail.getFileName();
+
+			String output = "File updated: " + GC_PATH + fileDetail.getFileName();
 			return Response.status(200).entity(output).build();	
-			
+
 		}
 		else
 		{
-			String output = "ERROR: Non-existing file cannot be updated : "+ UPLOAD_PATH + fileDetail.getFileName();
+			String output = "ERROR: Non-existing file cannot be updated : "+ GC_PATH + fileDetail.getFileName();
 			return Response.status(500).entity(output).build();	
 		}	
 	}	
@@ -111,20 +145,19 @@ public class FileResource {
 	@DELETE
 	@Path("/{fileName}")
 	public Response deleteFile(@PathParam("fileName") String fileName) {
-		//		fileService.removeFile(fileName);
 
 		boolean bool = false;
 
-		File file = new File(UPLOAD_PATH + fileName);
+		File file = new File(GC_PATH + fileName);
 		bool = file.delete();
 
 		if (bool)  {
-			String output = "File deleted : "+ UPLOAD_PATH + fileName;
+			String output = "File deleted : "+ GC_PATH + fileName;
 			return Response.status(200).entity(output).build();			
 		}
 		else
 		{
-			String output = "ERROR: File cannot be deleted : "+ UPLOAD_PATH + fileName;
+			String output = "ERROR: File cannot be deleted : "+ GC_PATH + fileName;
 			return Response.status(500).entity(output).build();	
 		}
 	}
--- gocic/src/main/webapp/index.jsp
+++ gocic/src/main/webapp/index.jsp
@@ -2,7 +2,9 @@
 <body>
     <h2>OCI/NSS Global Coordination RESTful Web Application!</h2>
     <p><a href="webapi/myresource">Jersey resource</a>
-    <p>Visit <a href="http://jersey.java.net">Project Jersey website</a>
-    for more information on Jersey!
+    
+    <p><a href="http://127.0.0.1:8080/gocic/fileUpload.html">File Upload</a>
+       
+    <p>Visit <a href="http://jersey.java.net">Project Jersey website</a>  for more information on Jersey!
 </body>
 </html>
