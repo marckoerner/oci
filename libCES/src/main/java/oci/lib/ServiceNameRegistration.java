b51d5250741eb0fd4d6025e975882f68a7fcdea5
marckoerner <koerner@icsi.berkeley.edu>
2017-08-23 17:00:56 -0700

    null pointer check workaround

--- libCES/src/main/java/oci/lib/ServiceNameRegistration.java
+++ libCES/src/main/java/oci/lib/ServiceNameRegistration.java
@@ -8,6 +8,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 /**
  * This class contains all methods for an edge service registration and un-registration at the local OCI coordinator
@@ -24,7 +25,7 @@ public class ServiceNameRegistration {
 	 * This method registers an edge service at the local OCI coordinator
 	 * @param serviceName Name of the edge service
 	 * @param ip IP address of the edge service
-	 * @return registration key if successful and 0 if an error appeared
+	 * @return registration key if successful and ServiceNameEntry.NO_ID if an error appeared
 	 */
 	public final static int registerEdgeService(String serviceName, InetAddress ip) {
 		
@@ -48,7 +49,7 @@ public class ServiceNameRegistration {
 		
 		try {
 			Socket locicSocket = new Socket(InetAddress.getByAddress(IPADDRESS), PORT);
-			locicSocket.setSoTimeout(SOCKET_TIMEOUT);
+			//locicSocket.setSoTimeout(SOCKET_TIMEOUT);
 			
 			ObjectOutputStream	oos = new ObjectOutputStream(locicSocket.getOutputStream());
 			ObjectInputStream	ois = new ObjectInputStream(locicSocket.getInputStream());			
@@ -80,7 +81,14 @@ public class ServiceNameRegistration {
 	 */
 	public final static boolean unregisterEdgeService(String serviceName, int registrationKey) {	
 		
-		ServiceNameEntry serviceNameEntry = new ServiceNameEntry(serviceName, null);
+		ServiceNameEntry serviceNameEntry = null;
+		try {
+			// workaround for safety on null pointer in registerEdgeService() method
+			serviceNameEntry = new ServiceNameEntry(serviceName, InetAddress.getByAddress(IPADDRESS));
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+			return false;
+		}
 		serviceNameEntry.setKey(registrationKey);	
 		return unregisterEdgeService(serviceNameEntry);	
 	}
@@ -93,7 +101,6 @@ public class ServiceNameRegistration {
 	public final static boolean unregisterEdgeService(ServiceNameEntry serviceNameEntry) {
 		
 		// TODO: check method parameters for safety
-		
 		boolean ret = true;
 		if(serviceNameEntry.getKey() == registerEdgeService(serviceNameEntry)) ret = false;
 		return ret;
