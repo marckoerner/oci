26533fd80c24d72d381a1d53c162984a2e5cea7a
marckoerner <koerner@icsi.berkeley.edu>
2017-08-27 20:56:21 -0700

    initial commit of the OCI resource and orchestration manager project

---
+++ oci-resource-manager/src/main/java/oci/rom/Manager.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package oci.rom;
+
+import java.util.logging.Logger;
+
+/**
+ * @author Marc Koerner
+ *
+ */
+public class Manager {
+	
+    static final Logger			LOGGER			= Logger.getLogger(Manager.class.getName());
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		LOGGER.info("Resource and orchestration manager started");
+
+		
+		
+		LOGGER.info("Resource and orchestration manager stopped");
+		
+	} // main
+
+}
---
+++ oci-resource-manager/src/main/java/oci/rom/MininetApi.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package oci.rom;
+
+/**
+ * This class is a wrapper implementation for the Mininet API 
+ * @author Marc Koerner
+ */
+public class MininetApi {
+	
+	/**
+	 * Starts a mininet host
+	 * @return IP address of the host
+	 */
+	final static String startMininetHost() {
+		String hostname = null;
+		return hostname;
+	}
+	
+	/**
+	 * Stops the given mininet host
+	 * @param hostname
+	 * @return true if success
+	 */
+	final static boolean stopMininetHost(String hostname) {
+		return true;
+	}
+
+}// class MininetApi
