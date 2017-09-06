9ab0c9fa5f6dc22cfa4148949d247069b54c5107
Torsten Runge <runge@icsi.berkeley.edu>
2017-09-06 15:37:44 -0700

    read and write GC stats from WebService

--- global-oci-coordinator/src/main/java/oci/gocic/GlobalOciCoordinator.java
+++ global-oci-coordinator/src/main/java/oci/gocic/GlobalOciCoordinator.java
@@ -5,7 +5,9 @@ package oci.gocic;
 
 import java.net.InetAddress;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Vector;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Logger;
 
@@ -25,74 +27,90 @@ public class GlobalOciCoordinator {
 	/**
 	 * @param args
 	 */
-	
+
 	static ConcurrentHashMap<String, LocalCoordinator> localCoordinators = new ConcurrentHashMap<>();
-	
-    private static final Logger LOGGER = Logger.getLogger(GlobalOciCoordinator.class.getName());
-		
+	static ConcurrentHashMap<String, Vector<String>> localCoordinatorFiles = new ConcurrentHashMap<>();
+
+	private static final Logger LOGGER = Logger.getLogger(GlobalOciCoordinator.class.getName());
+
 	public static void main(String[] args) throws Exception {
 		LOGGER.info("Logger Name: " + LOGGER.getName());
 		LOGGER.info("Global OCI Coordinator started");
-				
-        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-//		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.NO_SESSIONS);
-        context.setContextPath("/gc");
 
-        Server jettyServer = new Server(9999);
-        jettyServer.setHandler(context);
+		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+		//		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.NO_SESSIONS);
+		context.setContextPath("/gc");
 
-        ServletHolder jerseyServlet = context.addServlet(ServletContainer.class, "/*");
-        jerseyServlet.setInitOrder(0);
+		Server jettyServer = new Server(9999);
+		jettyServer.setHandler(context);
 
-        // Tells the Jersey Servlet which REST service/class to load.
-        jerseyServlet.setInitParameter("jersey.config.server.provider.packages", "oci.gocic");       
-        jerseyServlet.setInitParameter(ServerProperties.PROVIDER_CLASSNAMES, MultiPartFeature.class.getCanonicalName());
-        
-        // Example
-        // add some local coordinators      
-        addLocalCoordinator("LC1", InetAddress.getByName("127.0.0.1"));
-        addLocalCoordinator("LC2", InetAddress.getByName("127.0.0.2"));
-        addLocalCoordinator("LC3", InetAddress.getByName("127.0.0.3"));
-        
-        printAllLocalCoordinators();
-        
-        // Example end
+		ServletHolder jerseyServlet = context.addServlet(ServletContainer.class, "/*");
+		jerseyServlet.setInitOrder(0);
 
-        try {
-            jettyServer.start();
-            jettyServer.join();
-        }
-        finally {
-            jettyServer.destroy();
-        }
-				
+		// Tells the Jersey Servlet which REST service/class to load.
+		jerseyServlet.setInitParameter("jersey.config.server.provider.packages", "oci.gocic");       
+		jerseyServlet.setInitParameter(ServerProperties.PROVIDER_CLASSNAMES, MultiPartFeature.class.getCanonicalName());
+
+		// Example
+		// add some local coordinators      
+		addLocalCoordinator("LC1", InetAddress.getByName("127.0.0.1"));
+		addLocalCoordinator("LC2", InetAddress.getByName("127.0.0.2"));
+		addLocalCoordinator("LC3", InetAddress.getByName("127.0.0.3"));
+
+		printAllLocalCoordinators();
+
+		// Example end
+
+		try {
+			jettyServer.start();
+			jettyServer.join();
+		}
+		finally {
+			jettyServer.destroy();
+		}
+
 		LOGGER.info("Global OCI Coordinator stopped");
 
 	}
-	
+
 	public static List<LocalCoordinator> getAllLocalCoordinators() {
 		return new ArrayList<LocalCoordinator>(localCoordinators.values());		
 	}
-	
+
 	public static void printAllLocalCoordinators() {
-	    for (ConcurrentHashMap.Entry<String, LocalCoordinator> entry : localCoordinators.entrySet()) {
-	        String key = entry.getKey().toString();
-	        String value = entry.getValue().getIp().toString();
-	        System.out.println("key: " + key + " value: " + value);
-	      }
+		for (ConcurrentHashMap.Entry<String, LocalCoordinator> entry : localCoordinators.entrySet()) {
+			String key = entry.getKey().toString();
+			String value = entry.getValue().getIp().toString();
+			System.out.println("key: " + key + " value: " + value);
+		}
 
 	}
-	
+
+	public static void printAllLocalCoordinatorFiles() {
+		for (ConcurrentHashMap.Entry<String, Vector<String>> entry : localCoordinatorFiles.entrySet()) {
+			String key = entry.getKey().toString();
+			Vector<String> value = entry.getValue();
+
+			Iterator<String> itr = value.iterator();
+			while(itr.hasNext())
+			{      
+				System.out.println("key: " + key + " value: " + itr.next());
+			}     
+		}
+
+	}
+
 	public LocalCoordinator getLocalCoordinator(String name) {
 		return localCoordinators.get(name);
 	}
-	
+
 	public static LocalCoordinator addLocalCoordinator(String name, InetAddress ip) {
 		LocalCoordinator lc = new LocalCoordinator(name, ip);
 		localCoordinators.put(name, lc);
+		localCoordinatorFiles.put(name, new Vector<String>());
 		return lc;
 	}
-	
+
 	public LocalCoordinator removeLocalCoordinator(String name) {
 		return localCoordinators.remove(name);
 	}
