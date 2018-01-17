1ff6996191abbd05277cc038b2fa343d96b6e6bc
Torsten Runge <runge@icsi.berkeley.edu>
2018-01-17 15:30:26 -0800

    scheduler runs!

--- misc/ServiceNameResolverPerSec/src/main/java/oci/eval/ServiceNameRegistrationEvaluation/EvaluationServiceNameResolverPerSec.java
+++ misc/ServiceNameResolverPerSec/src/main/java/oci/eval/ServiceNameRegistrationEvaluation/EvaluationServiceNameResolverPerSec.java
@@ -17,37 +17,13 @@ import java.util.concurrent.*;
  * This application is a simple benchmark tool for the OCI name service 
  *
  */
-public class Evaluation implements Runnable
+
+public class EvaluationServiceNameResolverPerSec
 {
-	
-	static Vector<Long>	times	= new Vector<Long>();
-	static int		errors		= 0;
-	static int		entries			= 10;
-	
-	
-	public void run() {
-		
-		if(errors > 5) 
-		{
-			System.out.println("Too many getEdgeServiceIpAddress failures.");
-			return;
-		}
-			
-		
-    	// generate random service name within service boundaries
-    	Double d = Math.random() * entries;
-    	String serviceName = "service" + d.intValue();
-		
-		try {
-			long startTime	= System.nanoTime();
-			ServiceNameResolver.getEdgeServiceIpAddress(serviceName);
-			long stopTime	= System.nanoTime();
-			times.add(stopTime - startTime);
-		} catch(Exception error) {
-			error.printStackTrace();
-			errors++;
-		}
-	}
+
+	public static Vector<Long>	times	= new Vector<Long>();
+	public static int		errors		= 0;
+	public static int		entries			= 10;
 
 	public static void main( String[] args )
 	{
@@ -57,26 +33,12 @@ public class Evaluation implements Runnable
 
 		// delay in ms
 		//int		reg_delay		= 0;
-		// value separator in csv file
-		String	seperator		= ";";
 
 		String	serviceName	= null;
 		int		serviceKey	= ServiceNameEntry.NO_KEY;
 
-
-		File			samples	= new File("samples.csv");
-		FileWriter		fWriter = null;
-		try {
-			fWriter = new FileWriter(samples);
-		} catch (IOException e) {
-			e.printStackTrace();
-			return;
-		}
-		BufferedWriter	bWriter	= new BufferedWriter(fWriter);
-
-
 		// 1. pre work: feed LOCIC with edge service name entries
-		
+
 		for(int i = 0; i < entries; i++) {
 
 			//TODO: what is that?
@@ -101,40 +63,55 @@ public class Evaluation implements Runnable
 
 
 		// 2. actual work: benchmarking edge service name resolution to obtain edge service IP 
-		
-//		float loadStart = 1;
-//		float loadStep = 1;
-//		float loadEnd = 1;
-//
-//		for(float loadCurr = loadStart; loadCurr < loadEnd; loadCurr = loadCurr + loadStep) {
 
+//		int loadStart = 1; // number of loadCurr client requests per seconds
+//		int loadStep = 1;
+//		int loadEnd = 2;	
+		int loadCurr = 1;
+//		
+//		for(int loadCurr = loadStart; loadCurr < loadEnd; loadCurr = loadCurr + loadStep) {
 
-			ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
-			
-			int loadCurr = 1000; // 1000 client requests per seconds
+			ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 
-			Runnable task = new Evaluation();
-			long initialDelay = 500000000;
+			EvaluationTask evaluationTask = new EvaluationTask();
 			long periodicDelay = (long)(1000000000/loadCurr);
+			long initialDelay = periodicDelay;
 
-			final Future<?> f1 = scheduler.scheduleAtFixedRate(task, initialDelay, periodicDelay,TimeUnit.NANOSECONDS);
-			
-			
-		    Runnable cancelTask = new Runnable() {
-		        public void run() {
-		        	f1.cancel(true);
-		        }
-		    };
-			
-			scheduler.schedule(cancelTask, 5, TimeUnit.SECONDS);
-			
+			final Future<?> f1 = scheduler.scheduleAtFixedRate(evaluationTask, initialDelay, periodicDelay,TimeUnit.NANOSECONDS);
 
-//		}
+			Runnable cancelTask = new Runnable() {
+				public void run() {
+					f1.cancel(true);
+					EvaluationServiceNameResolverPerSec.doAfterwork();
+				}
+			};
 
-		// 3. after work: writes probes to file
+			scheduler.schedule(cancelTask, 10, TimeUnit.SECONDS);
 
+		}
+
+//		System.exit(0);
+
+//	}
+
+	// 3. after work: writes probes to file
+
+	public static void doAfterwork() {
 		float time_ms = 0;
-		
+		File			samples	= new File("samples.csv");
+		FileWriter		fWriter = null;
+		// value separator in csv file
+		String	seperator		= ";";
+
+
+		try {
+			fWriter = new FileWriter(samples);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
+		BufferedWriter	bWriter	= new BufferedWriter(fWriter);
+
 		try {
 			for(Long time : times) {
 				time_ms = (float) time / (float) 1000000;
@@ -152,6 +129,5 @@ public class Evaluation implements Runnable
 			return;
 		}
 
-		return;
 	} // main
 } // App
