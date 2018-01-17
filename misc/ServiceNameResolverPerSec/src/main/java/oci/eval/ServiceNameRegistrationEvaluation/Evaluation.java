e4df92a441125de46db268f734b86cfe6c991623
Torsten Runge <runge@icsi.berkeley.edu>
2018-01-16 17:24:15 -0800

    scheduler

--- misc/ServiceNameResolverPerSec/src/main/java/oci/eval/ServiceNameRegistrationEvaluation/Evaluation.java
+++ misc/ServiceNameResolverPerSec/src/main/java/oci/eval/ServiceNameRegistrationEvaluation/Evaluation.java
@@ -11,135 +11,147 @@ import oci.lib.ServiceNameEntry;
 import oci.lib.ServiceNameRegistration;
 import oci.lib.ServiceNameResolver;
 
+import java.util.concurrent.*;
+
 /**
  * This application is a simple benchmark tool for the OCI name service 
  *
  */
-public class Evaluation 
+public class Evaluation implements Runnable
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "LOCIC based Name Service Lookup Benchmark" );
-        
-        String	locic_ip		= "localhost";
-        int		entries			= 100;
-        int		probes			= 50;
-        int		probe_offset	= 0;
-        // delay in ms
-        int		reg_delay		= 0;
-        int		req_delay		= 0;
-        // value separator in csv file
-        String	seperator		= ";";
-        
-        long	startTime;
-        long	stopTime;
-        
-        String	serviceName	= null;
-        int		serviceKey	= ServiceNameEntry.NO_KEY;
-        int		errors		= 0;
-        
-        File			samples	= new File("samples.csv");
-        FileWriter		fWriter = null;
+	
+	static Vector<Long>	times	= new Vector<Long>();
+	static int		errors		= 0;
+	static int		entries			= 10;
+	
+	
+	public void run() {
+		
+		if(errors > 5) 
+		{
+			System.out.println("Too many getEdgeServiceIpAddress failures.");
+			return;
+		}
+			
+		
+    	// generate random service name within service boundaries
+    	Double d = Math.random() * entries;
+    	String serviceName = "service" + d.intValue();
+		
+		try {
+			long startTime	= System.nanoTime();
+			ServiceNameResolver.getEdgeServiceIpAddress(serviceName);
+			long stopTime	= System.nanoTime();
+			times.add(stopTime - startTime);
+		} catch(Exception error) {
+			error.printStackTrace();
+			errors++;
+		}
+	}
+
+	public static void main( String[] args )
+	{
+		System.out.println( "LOCIC based Name Service Lookup Benchmark" );
+
+		String	locic_ip		= "localhost";
+
+		// delay in ms
+		//int		reg_delay		= 0;
+		// value separator in csv file
+		String	seperator		= ";";
+
+		String	serviceName	= null;
+		int		serviceKey	= ServiceNameEntry.NO_KEY;
+
+
+		File			samples	= new File("samples.csv");
+		FileWriter		fWriter = null;
 		try {
 			fWriter = new FileWriter(samples);
 		} catch (IOException e) {
 			e.printStackTrace();
 			return;
 		}
-        BufferedWriter	bWriter	= new BufferedWriter(fWriter);
-        Vector<Long>	times	= new Vector<Long>();
-        
-        // feed LOCIC with edge service name entries
-        for(int i = 0; i < entries; i++) {
-        	
-        	if(errors > 5) return;
-        	
-        	serviceName = "test" + i;
-        	
-        	serviceKey = ServiceNameEntry.NO_KEY;
-    		try {
-    			startTime	= System.nanoTime();
-    			serviceKey	= ServiceNameRegistration.registerEdgeService(serviceName, InetAddress.getByName(locic_ip));
-    			stopTime	= System.nanoTime();
-    			times.add(stopTime - startTime);
-    			if(serviceKey == ServiceNameEntry.NO_KEY) {
-    				errors++;
-    			}
-    			Thread.sleep(reg_delay);
+		BufferedWriter	bWriter	= new BufferedWriter(fWriter);
 
-    		} catch(Exception error) {
-    			error.printStackTrace();
-    			errors++;
-    		}
-    		
-        }
-        
-        // write probes to file
-        float time_ms = 0;
-    	try {
-    		for(Long time : times) {
-    			time_ms = (float) time / (float) 1000000;
-				bWriter.write(String.format("%.3f", time_ms) + seperator);
-				System.out.print(String.format("%.3f", time_ms) + seperator);
-    		}
-    		bWriter.newLine();
-    		bWriter.flush();
-    		System.out.println();
-		} catch (IOException e) {
-			e.printStackTrace();
-			return;
+
+		// 1. pre work: feed LOCIC with edge service name entries
+		
+		for(int i = 0; i < entries; i++) {
+
+			//TODO: what is that?
+			if(errors > 5) return;
+
+			serviceName = "service" + i;
+
+			serviceKey = ServiceNameEntry.NO_KEY;
+			try {
+				serviceKey	= ServiceNameRegistration.registerEdgeService(serviceName, InetAddress.getByName(locic_ip));
+				if(serviceKey == ServiceNameEntry.NO_KEY) {
+					errors++;
+				}
+
+
+			} catch(Exception error) {
+				error.printStackTrace();
+				errors++;
+			}
+
 		}
-        // System.out.println( "Summary: " );
 
-        times = null;
-        times = new Vector<Long>();
-        
-        InetAddress edge_service_ip = null;
-        
-        int probe = 0 + probe_offset;
-    	        
-        // obtain edge service IP information
-        for(int i = 0; i < probes; i++) {
-        	
-        	if(errors > 5) return;
-        	
-            serviceName = "test" + probe;
-        	
-    		try {
-    			startTime	= System.nanoTime();
-    			edge_service_ip	= ServiceNameResolver.getEdgeServiceIpAddress(serviceName);
-    			stopTime	= System.nanoTime();
-    			times.add(stopTime - startTime);
-    			if(serviceKey == ServiceNameEntry.NO_KEY) {
-    				errors++;
-    			}
-    			Thread.sleep(req_delay); // ms
 
-    		} catch(Exception error) {
-    			error.printStackTrace();
-    			errors++;
-    		}
-    		
-        }
-        
-        // writes probes to file
-    	try {
-    		for(Long time : times) {
-    			time_ms = (float) time / (float) 1000000;
+		// 2. actual work: benchmarking edge service name resolution to obtain edge service IP 
+		
+//		float loadStart = 1;
+//		float loadStep = 1;
+//		float loadEnd = 1;
+//
+//		for(float loadCurr = loadStart; loadCurr < loadEnd; loadCurr = loadCurr + loadStep) {
+
+
+			ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+			
+			int loadCurr = 1000; // 1000 client requests per seconds
+
+			Runnable task = new Evaluation();
+			long initialDelay = 500000000;
+			long periodicDelay = (long)(1000000000/loadCurr);
+
+			final Future<?> f1 = scheduler.scheduleAtFixedRate(task, initialDelay, periodicDelay,TimeUnit.NANOSECONDS);
+			
+			
+		    Runnable cancelTask = new Runnable() {
+		        public void run() {
+		        	f1.cancel(true);
+		        }
+		    };
+			
+			scheduler.schedule(cancelTask, 5, TimeUnit.SECONDS);
+			
+
+//		}
+
+		// 3. after work: writes probes to file
+
+		float time_ms = 0;
+		
+		try {
+			for(Long time : times) {
+				time_ms = (float) time / (float) 1000000;
 				bWriter.write(String.format("%.3f",time_ms) + seperator);
 				System.out.print(String.format("%.3f", time_ms) + seperator);
-    		}
-    		bWriter.newLine();
-    		bWriter.flush();
-    		System.out.println();
-    		
-    		bWriter.close();
-    		fWriter.close();
+			}
+			bWriter.newLine();
+			bWriter.flush();
+			System.out.println();
+
+			bWriter.close();
+			fWriter.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 			return;
 		}
 
-        return;
-    } // main
+		return;
+	} // main
 } // App
