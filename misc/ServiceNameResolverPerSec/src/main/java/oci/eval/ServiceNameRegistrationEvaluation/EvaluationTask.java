99acfd1b2fecde1e077d49e595856c5820d6d07c
Torsten Runge <runge@icsi.berkeley.edu>
2018-01-22 13:38:15 -0800

    loadCurr iteration with automatic LOCIC start and kill

--- misc/ServiceNameResolverPerSec/src/main/java/oci/eval/ServiceNameRegistrationEvaluation/EvaluationTask.java
+++ misc/ServiceNameResolverPerSec/src/main/java/oci/eval/ServiceNameRegistrationEvaluation/EvaluationTask.java
@@ -4,8 +4,7 @@ import oci.lib.ServiceNameResolver;
 
 public class EvaluationTask extends Thread
 {
-	public void run() {
-		
+	public void run() {		
 		if(EvaluationServiceNameResolverPerSec.errors > 5) 
 		{
 			System.out.println("Too many getEdgeServiceIpAddress failures.");
@@ -15,6 +14,15 @@ public class EvaluationTask extends Thread
     	// generate random service name within service boundaries
     	Double d = Math.random() * EvaluationServiceNameResolverPerSec.entries;
     	String serviceName = "service" + d.intValue();
+    	
+    	// generate random start time to model poission arrival of client request
+    	Double randomStartTime_ms = Math.random() * (EvaluationServiceNameResolverPerSec.periodicDelay/1000000);
+
+		try {
+			Thread.sleep(randomStartTime_ms.intValue());
+		} catch (InterruptedException e) {
+			// nothing
+		}
 		
 		try {
 			long startTime	= System.nanoTime();
