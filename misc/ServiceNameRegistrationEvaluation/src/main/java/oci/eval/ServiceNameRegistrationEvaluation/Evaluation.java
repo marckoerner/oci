e848820630c592e8a5ae191da25229be7279c483
marckoerner <koerner@icsi.berkeley.edu>
2018-01-24 13:37:46 -0800

    2nd inter host test

--- misc/ServiceNameRegistrationEvaluation/src/main/java/oci/eval/ServiceNameRegistrationEvaluation/Evaluation.java
+++ misc/ServiceNameRegistrationEvaluation/src/main/java/oci/eval/ServiceNameRegistrationEvaluation/Evaluation.java
@@ -22,7 +22,7 @@ public class Evaluation
         System.out.println( "LOCIC based Name Service Lookup Benchmark" );
         
         // String	locic_ip		= "localhost";
-        String	locic_ip		= "10.142.36.78";
+        String	locic_ip		= "128.32.37.53";
         int		entries			= 200;
         int		probes			= 20;
         int		probe_offset	= 0;
