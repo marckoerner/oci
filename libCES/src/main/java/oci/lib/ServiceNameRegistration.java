ff53a211a95c3891877beb2905d49929d6f077f7
marckoerner <koerner@icsi.berkeley.edu>
2018-01-24 15:20:47 -0800

    added tcp_no_delay in order to avoid data grouping

--- libCES/src/main/java/oci/lib/ServiceNameRegistration.java
+++ libCES/src/main/java/oci/lib/ServiceNameRegistration.java
@@ -50,6 +50,7 @@ public class ServiceNameRegistration {
 		try {
 			Socket locicSocket = new Socket(InetAddress.getByAddress(IPADDRESS), PORT);
 			locicSocket.setSoTimeout(SOCKET_TIMEOUT);
+			locicSocket.setTcpNoDelay(true);
 			
 			ObjectOutputStream	oos = new ObjectOutputStream(locicSocket.getOutputStream());
 			ObjectInputStream	ois = new ObjectInputStream(locicSocket.getInputStream());			
