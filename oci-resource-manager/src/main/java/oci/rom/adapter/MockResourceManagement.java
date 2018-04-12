13378cdf8649ba8e348b5d3c2488cd18806d502b
marckoerner <koerner@icsi.berkeley.edu>
2018-04-11 17:42:07 -0700

    minor restructuring

--- oci-resource-manager/src/main/java/oci/rom/adapter/MockResourceManagement.java
+++ oci-resource-manager/src/main/java/oci/rom/adapter/MockResourceManagement.java
@@ -78,4 +78,10 @@ public class MockResourceManagement implements GenericResourceManagementInterfac
 		return false;
 	}
 
+	@Override
+	public boolean resourcesAvailable(File edgeServiceApplicationPackage) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
 }
