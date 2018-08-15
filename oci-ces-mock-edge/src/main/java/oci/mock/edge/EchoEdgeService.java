83341e690daa87f5859abc7ed2cfb7ad24e3166a
marckoerner <koerner@icsi.berkeley.edu>
2018-08-15 14:58:23 -0700

    cleanup

--- oci-ces-mock-edge/src/main/java/oci/mock/edge/EchoEdgeService.java
+++ oci-ces-mock-edge/src/main/java/oci/mock/edge/EchoEdgeService.java
@@ -5,7 +5,6 @@ package oci.mock.edge;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.logging.ConsoleHandler;
