dd369785d5b506281222f0b1b7da223c96fe19b9
marckoerner <koerner@icsi.berkeley.edu>
2018-08-24 15:24:16 +0200

    added constructor

--- global-oci-coordinator-v2/src/main/java/oci/gocic/Locic.java
+++ global-oci-coordinator-v2/src/main/java/oci/gocic/Locic.java
@@ -4,8 +4,14 @@ import java.net.InetAddress;
 
 public class Locic {
     
+    public Locic(InetAddress ip, String subnet, String location) {
+        this.ip         = ip;
+        this.subnet     = subnet;
+        this.location   = location;
+    }
+    
     private InetAddress ip          = null;
     private String      subnet      = null;
     private String      location    = null;
 
-}
+} // class
