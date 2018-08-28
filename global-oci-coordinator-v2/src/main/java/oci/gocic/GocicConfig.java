4ab4d9504cd2886eed1e50cad791a9910cf14651
marckoerner <koerner@icsi.berkeley.edu>
2018-08-28 02:08:28 +0200

    added config onject class

---
+++ global-oci-coordinator-v2/src/main/java/oci/gocic/GocicConfig.java
@@ -0,0 +1,49 @@
+
+package oci.gocic;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Vector;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+/**
+ * Object class of the GOCIC configuration file containing the parsed configuration information.
+ * @author Marc Koerner
+ */
+public class GocicConfig {
+
+    private File    configFile  = null;
+    private Vector  locics      = new Vector<Locic>();
+    private Vector  gocics      = new Vector<Locic>();
+    
+    public GocicConfig(File configFile) {
+        this.configFile = configFile;
+    }
+    
+    public boolean parseConfig() {
+        if(configFile == null) return false;
+        
+        try {
+            FileReader  fileReader  = new FileReader(this.configFile);
+            JSONParser  parser      = new JSONParser();
+            JSONObject  object      = (JSONObject) parser.parse(fileReader);
+            
+            String      ip          = (String) object.get("ip");
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        
+        return true;
+    }
+    
+    public boolean parseConfig(File configFile) {
+        this.configFile = configFile;
+        return this.parseConfig();
+    }
+    
+} // class
