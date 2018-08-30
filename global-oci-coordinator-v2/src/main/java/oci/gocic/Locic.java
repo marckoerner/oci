d7bde1828ec44c259b722b75bc9d40ef5d03c500
marckoerner <koerner@icsi.berkeley.edu>
2018-08-30 13:43:25 -0700

    added some java doc

--- global-oci-coordinator-v2/src/main/java/oci/gocic/Locic.java
+++ global-oci-coordinator-v2/src/main/java/oci/gocic/Locic.java
@@ -16,10 +16,10 @@ public class Locic {
     private String          location    = null;
     
     /**
-     * Constructor with CIDR subnet notation, e.g. 192.168.1.1/24
-     * @param ip
-     * @param cidrSubnet
-     * @param location
+     * Constructor with CIDR sub-net notation, e.g. 192.168.1.1/24
+     * @param ip LOCIC's IP address
+     * @param cidrSubnet sub-network in CIDR notation belonging the LOCIC's domain
+     * @param location location covered by the regarding central office (e.g. US.CA.Berkeley)
      */
     public Locic(InetAddress ip, String cidrSubnet, String location) {
         this.ip         = ip;
@@ -29,10 +29,10 @@ public class Locic {
     
     /**
      * Constructor with netmask notation, e.g. 192.168.1.1 255.255.255.o
-     * @param ip
-     * @param subnetIp
-     * @param subnetMask
-     * @param location
+     * @param ip LOCIC's IP address
+     * @param subnetIp sub-network IP
+     * @param subnetMask sub-network as IP mask belonging the LOCIC's domain
+     * @param location location covered by the regarding central office (e.g. US.CA.Berkeley)
      */
     public Locic(InetAddress ip, String subnetIp, String subnetMask, String location) {
         this.ip         = ip;
@@ -40,16 +40,27 @@ public class Locic {
         this.location   = location;
     }
     
+    /**
+     * Checks if an IP address belongs to the LOCIC's associated sub-network
+     * @param ip IP
+     * @return true if the given IP belongs to the sub-network
+     */
     public boolean belongsToSubnet(InetAddress ip) {   
         String      ip_string   = ip.getHostAddress();
         SubnetInfo  subnet      = this.subnet.getInfo();    
         return subnet.isInRange(ip_string);        
     }
     
+    /**
+     * @return returns the IP address of the LOCIC
+     */
     public InetAddress getIpAddr() {
         return this.ip;
     }
     
+    /**
+     * @return returns a String with the IP address of the LOCIC
+     */
     public String getIpString() {
         return this.ip.getHostAddress();
     }
