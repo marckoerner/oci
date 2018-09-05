5bfefe05a09bb1110708194b569af2834cdda989
marckoerner <koerner@icsi.berkeley.edu>
2018-09-04 17:32:27 -0700

    cleanup

--- global-oci-coordinator-v2/src/main/java/oci/gocic/LocicResolverWorker.java
+++ global-oci-coordinator-v2/src/main/java/oci/gocic/LocicResolverWorker.java
@@ -10,14 +10,12 @@ import java.net.InetAddress;
 import java.util.Iterator;
 
 /**
+ * An instance of a this class determines to which LOCIC a specific client belongs and sends its IP address
  * @author Marc Koerner
- *
  */
 public class LocicResolverWorker extends Thread {
     
     private DatagramSocket  udpServerSocket = null;
-    private boolean         running         = true;
-    private byte[]          buffer          = new byte[256]; // think about buffer / message size
     private GocicConfig     config          = null;
     private DatagramPacket  packet          = null;
     
@@ -32,21 +30,18 @@ public class LocicResolverWorker extends Thread {
         
         int             client_port     = 0;
         InetAddress     ip              = null;
-        byte[]          data            = null;
 
-        // extract client ip and port
         ip          = packet.getAddress();
         client_port = packet.getPort();
-        data        = new byte[packet.getLength()]; // check if this is necessary 
         
-        // check client sub-network
         Locic               locic   = null;
         Iterator<Locic>     itr     = this.config.getLocics().iterator();
         
+        // check if client belongs to a particular LOCIC's sub-network
         while(itr.hasNext()) {         
-            
             locic = (Locic) itr.next();
             if(locic.belongsToSubnet(ip)) {
+                // send LOCIC IP to client
                 DatagramPacket  reply = new DatagramPacket(locic.getIpString().getBytes(),
                                                             locic.getIpString().getBytes().length,
                                                             ip, client_port);
